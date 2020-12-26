84f0e0c1e7bd3bafe629a3dc2f21e7c3021bf519
Enrico <enricogit00@gmail.com>
2020-12-26 18:13:27 +0100

    lesson 19

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,28 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld/>
+    <h1>{{ title }}</h1>
+    <ninjas></ninjas>
+
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Ninjas from './components/Ninjas.vue'
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
+    'ninjas': Ninjas
+  },
+  data() {
+    return {
+      title: 'Ninja App'
+
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+// import Ninjas from './components/Ninjas.vue'
+// Vue.component('ninjas', Ninjas);
+
 new Vue({
   render: h => h(App),
 }).$mount('#app')
