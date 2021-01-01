f7cdef56843fc54d0c8872bca020243f895b55bc
Enrico <enricogit00@gmail.com>
2021-01-01 23:51:26 +0100

    lesson 34

--- src/main.js
+++ src/main.js
@@ -4,7 +4,28 @@ import VueResource from 'vue-resource'
 
 Vue.use(VueResource);
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+//Custom directives
+Vue.directive('rainbow', {
+  bind(el) {
+    el.style.color = "#" + Math.random().toString().slice(2, 8);
+  }
+});
+
+Vue.directive('theme', {
+  bind(el, binding) {
+    if (binding.value == 'wide') {
+      el.style.maxWidth = "1200px"
+    } else if (binding.value == 'narrow') {
+      el.style.maxWidth = "560px"
+    }
+    if (binding.arg == 'column') {
+      el.style.background = '#ddd';
+      el.style.padding = '20px';
+    }
+  }
+})
 
 new Vue({
   render: h => h(App),
