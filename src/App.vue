146857c60476e4f22e765ed0f0a069ca094ac494
Enrico <enricogit00@gmail.com>
2021-01-02 12:06:32 +0100

    lesson 41

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,23 @@
 <template>
   <div>
+    <app-header></app-header>
     <router-view></router-view>
   </div>
 </template>
 
 <script>
-/* import addBlog from './components/addBlog.vue'
-import showBlogs from './components/showBlogs.vue'
-import listBlogs from './components/listBlogs.vue' */
+// import addBlog from './components/addBlog.vue'
+// import showBlogs from './components/showBlogs.vue'
+// import listBlogs from './components/listBlogs.vue'
+import header from './components/header.vue'
 
 export default {
-/*   components:{
-    'add-blog': addBlog,
-    'show-blogs': showBlogs,
-    'list-blogs': listBlogs
-  }, */
+  components:{
+    // 'add-blog': addBlog,
+    // 'show-blogs': showBlogs,
+    // 'list-blogs': listBlogs,
+    'app-header': header
+  },
   data() {
     return {
 
